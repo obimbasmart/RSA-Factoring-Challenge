a56d6ddaa0ff9c0d7fa804896cc44badb7951309
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-12-22 22:09:03 +0000

    code cleanup

---
+++ factors.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+This module aims to solve the RSA refactoring challenge
+"""
+
+import sys
+import ctypes
+
+path = "./lib_my_c_lib.so"    # set library path
+lib = ctypes.CDLL(path)		  # init library
+
+# set argtype to avoid overflow
+lib.smallest_divisor.argtypes = [ctypes.c_long]
+
+
+def main():
+    """driver program"""
+    if len(sys.argv) != 2:
+        return
+
+    try:
+        with open(sys.argv[1], 'r') as line:
+            num = line.readline()
+            while num != '':
+                num_int = int(num)
+                divisor = lib.smallest_divisor(num_int)
+                print("{}={}*{}".format(num_int, num_int // divisor, divisor))
+                num = line.readline()
+    except FileNotFoundError:
+        return
+
+
+main()
